c7899843d1618e152d69c1db4a183fd53a587d96
PYRO DRYAD 69 <84041415+SLYAER@users.noreply.github.com>
2026-03-27 01:40:34 +0530

    Implement leveling system for guild users

--- systems/leveling.js
+++ systems/leveling.js
@@ -1,1 +1,29 @@
 
+const Guild = require("../models/Guild");
+
+module.exports = (client) => {
+  client.on("messageCreate", async (msg) => {
+    if (msg.author.bot) return;
+
+    let guild = await Guild.findOne({ guildId: msg.guild.id });
+    if (!guild) return;
+
+    if (!guild.users) guild.users = {};
+
+    if (!guild.users[msg.author.id]) {
+      guild.users[msg.author.id] = { xp: 0, level: 1 };
+    }
+
+    let user = guild.users[msg.author.id];
+
+    user.xp += 5;
+
+    if (user.xp >= 100) {
+      user.level++;
+      user.xp = 0;
+      msg.channel.send(`${msg.author} leveled up`);
+    }
+
+    await guild.save();
+  });
+};
