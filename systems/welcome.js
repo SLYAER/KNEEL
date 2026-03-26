f457e4c871a25d544993d6c00f5088c4379ecc7c
PYRO DRYAD 69 <84041415+SLYAER@users.noreply.github.com>
2026-03-27 01:40:12 +0530

    Implement welcome and leave messages for members
    Add welcome and farewell messages for guild members.

--- systems/welcome.js
+++ systems/welcome.js
@@ -1,1 +1,20 @@
+const Guild = require("../models/Guild");
 
+module.exports = (client) => {
+  client.on("guildMemberAdd", async (member) => {
+    const guild = await Guild.findOne({ guildId: member.guild.id });
+    if (!guild) return;
+
+    const msg = (guild.welcomeMessage || "Welcome {user}")
+      .replace("{user}", member);
+
+    member.guild.systemChannel?.send(msg);
+  });
+
+  client.on("guildMemberRemove", async (member) => {
+    const guild = await Guild.findOne({ guildId: member.guild.id });
+    if (!guild) return;
+
+    member.guild.systemChannel?.send(`😢 ${member.user.tag} left`);
+  });
+};
