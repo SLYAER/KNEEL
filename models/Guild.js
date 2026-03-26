826d38dc2601c7ce30b122d8d74d5fbccabb9bf7
PYRO DRYAD 69 <84041415+SLYAER@users.noreply.github.com>
2026-03-27 01:49:32 +0530

    Enhance Guild schema with additional fields
    Added fields for autoRole, welcomeMessage, leaveMessage, warnLimit, punishment, users, and warns to the guild schema.

--- models/Guild.js
+++ models/Guild.js
@@ -1,16 +1,24 @@
-
 const mongoose = require("mongoose");
 
 const guildSchema = new mongoose.Schema({
   guildId: String,
+
   autoRole: String,
+
   welcomeMessage: String,
+  leaveMessage: String,
 
   anti: {
     spam: Boolean,
     links: Boolean,
     badWords: Boolean
-  }
+  },
+
+  warnLimit: Number,
+  punishment: String,
+
+  users: Object,
+  warns: Object
 });
 
 module.exports = mongoose.model("Guild", guildSchema);
