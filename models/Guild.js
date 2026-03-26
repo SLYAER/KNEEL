7657fee9f9024578151ddb5ec74ffb8823e54ac8
PYRO DRYAD 69 <84041415+SLYAER@users.noreply.github.com>
2026-03-27 01:33:48 +0530

    Add mongoose schema for Guild model

--- models/Guild.js
+++ models/Guild.js
@@ -1,1 +1,16 @@
 
+const mongoose = require("mongoose");
+
+const guildSchema = new mongoose.Schema({
+  guildId: String,
+  autoRole: String,
+  welcomeMessage: String,
+
+  anti: {
+    spam: Boolean,
+    links: Boolean,
+    badWords: Boolean
+  }
+});
+
+module.exports = mongoose.model("Guild", guildSchema);
