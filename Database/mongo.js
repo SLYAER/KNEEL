707cfeb31a7e4fde0a7094f833109c7ab1d88c89
PYRO DRYAD 69 <84041415+SLYAER@users.noreply.github.com>
2026-03-27 03:50:41 +0530

    Implement try-catch for MongoDB connection
    Add error handling for MongoDB connection

--- Database/mongo.js
+++ Database/mongo.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 module.exports = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log("🟢 MongoDB Connected");
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("🟢 MongoDB Connected");
+  } catch (err) {
+    console.error("❌ MongoDB Connection Error:", err.message);
+  }
 };
