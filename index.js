12aeb75adddf9b4ebe9766314dbd62aa12caa1ab
PYRO DRYAD 69 <84041415+SLYAER@users.noreply.github.com>
2026-03-26 23:17:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+require("dotenv").config();
+const { Client, GatewayIntentBits } = require("discord.js");
 
+const client = new Client({
+  intents: Object.values(GatewayIntentBits)
+});
+
+client.on("ready", () => {
+  console.log(`KNEEL bot is online`);
+
+  client.user.setPresence({
+    activities: [{ name: "KNEEL Security", type: 3 }],
+    status: "online"
+  });
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "!ping") {
+    message.reply("KNEEL ⚔️ is active.");
+  }
+});
+
+client.login(process.env.TOKEN);
