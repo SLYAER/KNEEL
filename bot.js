a48142fb80a185b4aef1a91e9df38e425a609d8d
PYRO DRYAD 69 <84041415+SLYAER@users.noreply.github.com>
2026-03-27 14:13:55 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+console.log("🔥 BOT FILE STARTED");
 // ================== IMPORTS ==================
 const { addWarn, getWarns, resetWarns } = require("./utils/warnSystem");
 const { isBadWord } = require("./filters/badWords");
